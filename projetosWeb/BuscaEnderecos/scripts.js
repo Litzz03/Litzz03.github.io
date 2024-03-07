6e6defecf48460d2befb0690d3a4f4dc5fea0f74
Litzz03 <91480903+Litzz03@users.noreply.github.com>
2024-03-07 20:13:32 -0300

    Melhorias no projeto

--- projetosWeb/BuscaEnderecos/scripts.js
+++ projetosWeb/BuscaEnderecos/scripts.js
@@ -7,18 +7,32 @@ $(document).ready(function(){
     function consultaCep(){
         const cep = $('#cep').val();
         if(cep.length !== 8){
-            alert('Digite o cep corretamente');
+            $('#resultados').html('<h2 class="mensagemErro"> Digite o CEP corretamente!!!</h2>')
             return false;
         }
 
-        $.ajax({
-            url: 'https://viacep.com.br/ws/'+ cep +'/json/',
-            type: 'GET',
-            dataJson: 'json',
-            success: function(data){
-                console.log(data);
-            }
-        });
+        loading();
+        setTimeout(function(){
+            $.ajax({
+                url: 'https://viacep.com.br/ws/'+ cep +'/json/',
+                type: 'GET',
+                dataJson: 'json',
+                success: function(data){
+                    $('body').find('#resultados').html('<h2>Resultado da consulta</h2>'
+                                                        + '<p>CEP: '+data.cep+'</p>'
+                                                        +'<p>Logradouro: '+data.logradouro+'</p>'
+                                                        +'<p>Bairro: '+data.bairro+'</p>'
+                                                        +'<p>Cidade: '+data.localidade+'</p>'
+                                                        +'<p>Estado: '+data.uf+'</p>'
+                                                        +'<p>IBGE: '+data.ibge+'</p>'
+                                                        );
+                }
+            });
+        }, 4000);
+        
     };
 
+    function loading(){
+        $('#resultados').html('<img src="imagens/loading.gif" />')
+    }
 })
