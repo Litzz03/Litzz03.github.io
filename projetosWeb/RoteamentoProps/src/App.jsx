b95c9d833cee83a491f83aaf849f59f95576d549
Litzz03 <91480903+Litzz03@users.noreply.github.com>
2024-03-26 20:52:21 -0300

    RoteamentoProps - Commit Inicial

---
+++ projetosWeb/RoteamentoProps/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+const App = () => {
+
+  return(
+    <>
+      <Header/>
+      <main>Conteudo</main>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
---
+++ projetosWeb/RoteamentoProps/src/components/Header.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Header = () => {
+
+    return <header>Titulo</header>;
+};
+
+export default Header;
